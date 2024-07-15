621db5255fd1f362099cec6113669d0e782291a5
prajwalgowdang <102273957+prajwalng@users.noreply.github.com>
2024-07-15 20:26:44 +0530

    Delete Untitled8.m

--- Untitled8.m
+++
@@ -1,6 +0,0 @@
-% to display the data points with small circles,
-% use plot(x,y,'o') and combine the 2 plots with the
-% command plot(x,y,x,y,'o')
-
-plot(x,sin(x),x,sin(x),'o')
-xlabel('x'),ylabel('sin(x)')
