0f9bf7eeecc352bb5adcad713740f45dac1a5692
prajwalgowdang <102273957+prajwalng@users.noreply.github.com>
2025-07-12 12:38:34 +0530

    Add files via upload

---
+++ CLOTHING WEBSITE/src/Data.js
@@ -0,0 +1,30 @@
+let shopItemsData = [
+  {
+    id: "jfhgbvnscs",
+    name: "Casual Shirt",
+    price: 45,
+    desc: "Good to wear.",
+    img: "images/img-1.jpg",
+  },
+  {
+    id: "ioytrhndcv",
+    name: "Office Shirt",
+    price: 100,
+    desc: "Nice look in formal.",
+    img: "images/img-2.jpg",
+  },
+  {
+    id: "wuefbncxbsn",
+    name: "T Shirt",
+    price: 25,
+    desc: "White color.",
+    img: "images/img-3.jpg",
+  },
+  {
+    id: "thyfhcbcv",
+    name: "Mens Suit",
+    price: 300,
+    desc: "Black color.",
+    img: "images/img-4.jpg",
+  },
+];
